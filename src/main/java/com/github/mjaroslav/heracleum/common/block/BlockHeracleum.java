e5ced06d3321303f07a7ab10cfd23e1419d003b9
Yaroslav Novitsky <rcr3211@gmail.com>
2022-07-13 20:17:54 +0300

    refactor: make heracleum block is plant like block, WIP

--- src/main/java/com/github/mjaroslav/heracleum/common/block/BlockHeracleum.java
+++ src/main/java/com/github/mjaroslav/heracleum/common/block/BlockHeracleum.java
@@ -1,7 +1,18 @@
 package com.github.mjaroslav.heracleum.common.block;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+import org.jetbrains.annotations.UnknownNullability;
+
+import com.github.mjaroslav.heracleum.common.init.ModBlocks;
 import com.github.mjaroslav.heracleum.common.item.ItemBlockWithMetadata;
 import com.github.mjaroslav.heracleum.common.tileentity.TileEntityHeracleum;
+import com.google.common.collect.Lists;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import lombok.val;
@@ -16,31 +27,30 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Range;
-import org.jetbrains.annotations.UnknownNullability;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.IShearable;
+import net.minecraftforge.common.util.ForgeDirection;
 
-import java.util.List;
-
-public class BlockHeracleum extends ModBlockContainer<TileEntityHeracleum> {
+public class BlockHeracleum extends ModBlockContainer<TileEntityHeracleum> implements IPlantable, IShearable {
     public BlockHeracleum() {
         super("heracleum", Material.plants, TileEntityHeracleum.class, ItemBlockWithMetadata.class);
+        // TODO: Add ItemBlock icon or model
         setBlockTextureName("minecraft:tallgrass");
         setStepSound(Block.soundTypeGrass);
         setCreativeTab(CreativeTabs.tabDecorations);
+        setTickRandomly(true);
     }
 
     @Override
-    public void addCollisionBoxesToList(@NotNull World world, int x, int y, int z, @NotNull AxisAlignedBB bb,
-                                        @NotNull List list, @NotNull Entity entity) {
-        // TODO: WFT?
+    public void addCollisionBoxesToList(@NotNull World world, int x, int y, int z, @NotNull AxisAlignedBB bb, @NotNull List list, @NotNull Entity entity) {
+        // TODO: rewrite this, it's not work properly 
         val meta = world.getBlockMetadata(x, y, z);
-        if (meta > 0) {
-            if (meta != 3)
-                setBlockBounds(0.4375F, 0, 0.4375F, 0.5625F, 1, 0.5625F);
-        } else
-            setBlockBounds(0.46875F, 0, 0.46875F, 0.53125F, 0.5F, 0.53125F);
-        if (meta != 3)
+        if (meta > 0 && meta < 4)
+            setBlockBounds(0.5F - 1F / 16F, 0F, 0.5F - 1F / 16F, 0.5F + 1F / 16F, meta == 3 ? 0.5F : 1F, 0.5F + 1F / 16F);
+        else if (meta == 0)
+            setBlockBounds(0.5F - 0.5F / 16F, 0F, 0.5F - 0.5F / 16F, 0.5F + 0.5F / 16F, 0.5F, 0.5F + 0.5F / 16F);
+        else
             super.addCollisionBoxesToList(world, x, y, z, bb, list, entity);
     }
 
@@ -84,8 +94,7 @@ public class BlockHeracleum extends ModBlockContainer<TileEntityHeracleum> {
     }
 
     @Override
-    public TileEntityHeracleum createNewTileEntity(@NotNull World world,
-                                                   @Range(from = 0, to = 15) int metadata) {
+    public TileEntityHeracleum createNewTileEntity(@NotNull World world, @Range(from = 0, to = 15) int metadata) {
         return new TileEntityHeracleum();
     }
 
@@ -99,5 +108,73 @@ public class BlockHeracleum extends ModBlockContainer<TileEntityHeracleum> {
     @Override
     public int getRenderColor(@Range(from = 0, to = 16) int meta) {
         return getBlockColor();
+    }
+
+    @Override
+    public EnumPlantType getPlantType(@NotNull IBlockAccess world, int x, int y, int z) {
+        return EnumPlantType.Plains;
+    }
+
+    @Override
+    public Block getPlant(@NotNull IBlockAccess world, int x, int y, int z) {
+        return this;
+    }
+
+    @Override
+    public int getPlantMetadata(@NotNull IBlockAccess world, int x, int y, int z) {
+        return world.getBlockMetadata(x, y, z);
+    }
+
+    @Override
+    public boolean canPlaceBlockAt(@NotNull World world, int x, int y, int z) {
+        return super.canPlaceBlockAt(world, x, y, z) && canBlockStay(world, x, y, z);
+    }
+
+    @Override
+    public void onNeighborBlockChange(@NotNull World world, int x, int y, int z, Block block) {
+        super.onNeighborBlockChange(world, x, y, z, block);
+        checkAndDropBlock(world, x, y, z);
+    }
+
+    @Override
+    public void updateTick(@NotNull World world, int x, int y, int z, @NotNull Random rand) {
+        checkAndDropBlock(world, x, y, z);
+    }
+
+    @Override
+    public boolean canBlockStay(@NotNull World world, int x, int y, int z) {
+        return world.getBlock(x, y - 1, z).canSustainPlant(world, x, y - 1, z, ForgeDirection.UP, this);
+    }
+
+    protected void checkAndDropBlock(@NotNull World world, int x, int y, int z) {
+        if (!canBlockStay(world, x, y, z)) {
+
+            // TODO: rewrite this, it's not work properly 
+            dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+
+            world.setBlock(x, y, z, getBlockById(0), 0, 2);
+        }
+    }
+
+    @Override
+    public boolean canSustainPlant(@NotNull IBlockAccess world, int x, int y, int z, @NotNull ForgeDirection direction, @NotNull IPlantable plantable) {
+        // TODO: rewrite this, it's not work properly 
+        val plant = plantable.getPlant(world, x, y + 1, z);
+        if (plant == ModBlocks.heracleum && this == ModBlocks.heracleum) {
+            val meta = world.getBlockMetadata(x, y, z);
+            val plantMeta = plantable.getPlantMetadata(world, x, y + 1, z);
+            return (meta == 1 || meta == 2) && (plantMeta == 2 || plantMeta == 3);
+        }
+        return super.canSustainPlant(world, x, y, z, direction, plantable);
+    }
+
+    @Override
+    public boolean isShearable(@NotNull ItemStack item, @NotNull IBlockAccess world, int x, int y, int z) {
+        return true;
+    }
+
+    @Override
+    public @NotNull ArrayList<ItemStack> onSheared(@NotNull ItemStack item, @NotNull IBlockAccess world, int x, int y, int z, int fortune) {
+        return Lists.newArrayList(new ItemStack(this, 1, world.getBlockMetadata(x, z, z)));
     }
 }
